340190fa9cc83050518c00d808df9455dda1ca52
lahuke <luke@luketucker.com>
2017-08-25 10:21:26 -0600

    final

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-for (var i = 1; i <=100; i++) {
-    console.log(i)}		
-});
+	document.write("FizzBuzz Challenge", "<br>")
+for (var i = 1; i <=100; i++) {	
+if (i % 15 === 0){
+	document.write("FizzBuzz", "<br>");
+}
+else if (i % 5 === 0){
+	document.write("Buzz", "<br>");
+} 
+else if (i % 3 === 0){
+	document.write("Fizz" , "<br>");
+}
+else {
+	document.write(i, "<br>");
+}
+}
+});	
