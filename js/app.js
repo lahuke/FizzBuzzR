9417efada70e1b2da6fa60cb2d050b1f6cdfddaf
lahuke <luke@luketucker.com>
2017-08-30 12:56:37 -0600

    commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
-	document.write("FizzBuzz Challenge", "<br>")
-for (var i = 1; i <=100; i++) {	
-if (i % 15 === 0){
-	document.write("FizzBuzz", "<br>");
-}
-else if (i % 5 === 0){
-	document.write("Buzz", "<br>");
-} 
-else if (i % 3 === 0){
-	document.write("Fizz" , "<br>");
-}
-else {
-	document.write(i, "<br>");
-}
-}
-});	
+
+	function countNumbers(maxVal){
+    for (var count = 1; count <= maxVal; count++){
+      if (count % 15 == 0){
+        $('.content').append('FizzBuzz <br>');
+      }
+     else if (count % 3 == 0){
+        $('.content').append('Fizz <br>');
+      }
+     else if (count % 5 == 0){
+        $('.content').append('Buzz <br>');
+      }
+     else{
+        $('.content').append(count + ' <br>');
+      }
+    }
+  }
+  
+  //On click the magic happens
+  $('.playButton').submit(function(event){
+  	event.preventDefault();
+    var getValue = $('.textInput').val();
+    
+   //Checking if the typed value is a number
+    if (getValue % 1 == 0 && getValue <= 100){
+      $('.content').empty();
+    	countNumbers(getValue);
+    	$('.textInput').val("");
+    }else{
+      alert('Enter a number between 1 and 100.');
+      $('.textInput').val("");
+    }
+  });
+});
+
+ 
